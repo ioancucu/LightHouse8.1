430d18bb2f7e0d62fb0dccd7e5c4863491b0eadc
ioancucu <ioancucu84@gmail.com>
2023-07-25 17:46:49 +0200

    6544

--- handwritingRecognition.py
+++ handwritingRecognition.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 class VisioLH:
     def create_model():
         model = tf.keras.Sequential([
-            tf.keras.layers.Dense(784, activation='relu', input_shape=(784,)),  # Hidden layer with 128 nodes
+            tf.keras.layers.Dense(128, activation='relu', input_shape=(784,)),  # Hidden layer with 128 nodes
             tf.keras.layers.Dense(10, activation='softmax')  # Output layer with 10 nodes (for 10 classes)
         ])
 
