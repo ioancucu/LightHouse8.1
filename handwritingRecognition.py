f45978b1a62eeb24ca59fe7d9337aa762ad16d44
ioancucu <ioancucu84@gmail.com>
2023-07-25 17:35:18 +0200

    1111

--- handwritingRecognition.py
+++ handwritingRecognition.py
@@ -1,20 +1,10 @@
-import numpy as np
+import tensorflow as tf
 
-class NeuralNetwork:
-    def __init__(self, x, y):
-        self.input      = x
-        print (self.input.shape[0])
-        self.weights1   = np.random.rand(784,784) 
-        print (self.weights1)
-        self.weights2   = np.random.rand(4,1)   
-        self.y          = y
-        self.output     = np.zeros(self.y.shape)
+class VisioLH:
+    def create_model():
+        model = tf.keras.Sequential([
+            tf.keras.layers.Dense(784, activation='relu', input_shape=(784,)),  # Hidden layer with 128 nodes
+            tf.keras.layers.Dense(10, activation='softmax')  # Output layer with 10 nodes (for 10 classes)
+        ])
 
-if __name__ == "__main__":
-    X = np.array([[0,1,0],
-                  [1,1,0],
-                  [0,1,0],
-                  [1,1,1]])
-    
-    y = np.array([[0],[1],[0],[1]])
-    nn = NeuralNetwork(X,y)
+        return model
