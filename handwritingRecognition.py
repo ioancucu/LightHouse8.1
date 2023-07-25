7a1bdb8145a5c9f5c18ca2a897cf4b7175222aa0
ioancucu <ioancucu84@gmail.com>
2023-07-25 17:24:11 +0200

    1223

--- handwritingRecognition.py
+++ handwritingRecognition.py
@@ -3,7 +3,8 @@ import numpy as np
 class NeuralNetwork:
     def __init__(self, x, y):
         self.input      = x
-        self.weights1   = np.random.rand(self.input.shape[1],4) 
+        print (self.input.shape[0])
+        self.weights1   = np.random.rand(784,784) 
         print (self.weights1)
         self.weights2   = np.random.rand(4,1)   
         self.y          = y
